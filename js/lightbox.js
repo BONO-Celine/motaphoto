12c8b4b025f41a6431d47bd0c7e0f3d07775d0c7
Céline BONO <celine.bono@gmail.com>
2023-12-31 10:03:14 +0100

    Lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,91 @@
+
+ /**********************************************
+   * CODE LIGHTBOX 
+   **********************************************/
+var $ = jQuery;
+var currentIndex = 0;
+var postsData = [];
+
+function removeDuplicates(array) {
+    return array.filter((value, index, self) => {
+        return self.findIndex(v => v.imageUrl === value.imageUrl) === index;
+    });
+}
+
+function initializeLightboxData() {
+    postsData = removeDuplicates(lightboxData || []);
+}
+
+// Fonction pour ouvrir la lightbox avec l'image correspondante
+function openLightbox(imageUrl, imageRef, imageCategorie) {
+    var lightbox = $("#lightbox");
+    var lightboxImage = $("#lightbox-image");
+    var lightboxRef = $("#lightbox__ref");
+    var lightboxCategorie = $("#lightbox__categorie");
+
+    lightboxImage.attr("src", imageUrl);
+    lightboxRef.text(imageRef);
+    lightboxCategorie.text(imageCategorie);
+
+    lightbox.css("display", "flex"); // Changer diplay:none en flex pour afficher la lightbox
+}
+
+// Fonction pour fermer la lightbox
+function closeLightbox() {
+    var lightbox = $("#lightbox");
+    lightbox.css("display", "none");
+}
+
+// Exécuter la fonction lorsque le document est prêt
+$(document).ready(function () {
+    initializeLightboxData();
+
+    // Sélectionner tous les boutons avec la classe "icon-full-screen__button"
+    var fullscreenButtons = $(".icon-full-screen__button");
+
+    // Ajouter un écouteur d'événements à chaque bouton
+    fullscreenButtons.on("click", function () {
+        // Obtenir les données nécessaires à partir des attributs de données ou d'autres attributs du bouton
+        var imageUrl = $(this).data("image-url");
+        var imageRef = $(this).data("image-ref");
+        var imageCategorie = $(this).data("image-categorie");
+
+        // Réinitialiser le tableau avant de l'actualiser avec les nouvelles données
+        initializeLightboxData();
+
+        // Appeler la fonction openLightbox avec les données récupérées
+        openLightbox(imageUrl, imageRef, imageCategorie);
+    });
+    console.log(postsData); // Ajout de cette ligne pour vérifier les données
+    // Fermer la lightbox au clic sur la croix de fermeture
+    $(".close-lightbox").on("click", function () {
+        closeLightbox();
+    });
+    
+    // Événements clic sur les flèches
+    $("#lightbox-left-navigation").on("click", function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = postsData.length - 1;
+        }
+        openLightbox(
+            postsData[currentIndex].imageUrl,
+            postsData[currentIndex].imageRef,
+            postsData[currentIndex].imageCategorie
+        );
+    });
+
+    $("#lightbox-right-navigation").on("click", function () {
+        if (currentIndex < postsData.length - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        openLightbox(
+            postsData[currentIndex].imageUrl,
+            postsData[currentIndex].imageRef,
+            postsData[currentIndex].imageCategorie
+        );
+    });
+});
---
+++ templates-parts/lightbox.php
@@ -0,0 +1,18 @@
+<div id="lightbox" class="lightbox">
+  <button class="close-lightbox">&#10539;</button>
+  <div class="lightbox-content">
+    <!--<div class="arrow-left" id="arrow-left">&#10229 <span class="navigation-text">Précédente</span></div>-->
+    <!--<div class="previous-img-link"><span class="previous-img-arrow"></span>Précédente</div>-->
+    <div class="lightbox-left-navigation" id="lightbox-left-navigation"><span class="previous-img-arrow"></span><span class="previous-img-link">Précédente</span></div>
+    <div class="vertical-container">
+      <img id="lightbox-image" class="lightbox-image" src="" alt="Image en plein écran">
+      <div class="img-infos">
+        <span id="lightbox__ref" class="lightbox__ref"></span>
+        <span id="lightbox__categorie" class="lightbox__categorie"></span>
+      </div>
+    </div>
+    <!--<div class="arrow-right" id="arrow-right"><span class="navigation-text">Suivante</span> &#10230</div>-->
+    <!--<div class="next-img-link">Suivante<span class="next-img-arrow"></span></div>-->
+    <div class="lightbox-right-navigation" id="lightbox-right-navigation"><span class="next-img-link">Suivante</span><span class="next-img-arrow"></span></div>
+  </div>
+</div>
